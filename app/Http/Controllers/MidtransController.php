0a97de5c8d63953a6bd72513bb9b3c7b2a05daba
elanghairilanam <elang6414@gmail.com>
2022-11-17 15:31:48 +0700

    update

--- app/Http/Controllers/MidtransController.php
+++ app/Http/Controllers/MidtransController.php
@@ -11,98 +11,111 @@ use Midtrans\Notification;
 
 class Midtranscontroller extends Controller
 {
-    public function notificationHandler(Request $request){
-        //set config midtrans
+    // public function notificationHandler(Request $request){
+    //     //set config midtrans
+    //     Config::$serverKey = config('midtrans.serverKey');
+    //     Config::$isProduction = config('midtrans.isProduction');
+    //     Config::$isSanitized = config('midtrans.isSanitized');
+    //     Config::$is3ds = config('midtrans.is3ds');
+
+    //     //buat instance midtrans notification
+    //     $notification = new Notification();
+
+    //     //pisah order id dari string
+    //     $order = explode('-', $notification->order_id);
+
+    //     //assign ke variable
+    //     $status = $notification->transaction_status;
+    //     $type = $notification->payment_type;
+    //     $fraud = $notification->fraud_status;
+    //     $order_id = $order[1];
+
+    //     //cari transaksi berdasarkan id
+    //     $transaction = Transaction::findOrFail($order_id);
+
+    //     //handle notification status midtrans
+    //     if($status == 'capture'){
+    //         if($type == 'credit_card'){
+    //             if($fraud == 'challenge'){
+    //                 $transaction->transaction_status = 'CHALLENGE';
+    //             } else{
+    //                 $transaction->transaction_status = 'SUCCESS';
+    //             }
+    //         }
+    //     }
+    //     else if ($status == 'settlement') {
+    //         $transaction->transaction_status = 'SUCCESS';
+    //     }
+    //     else if ($status == 'pending') {
+    //         $transaction->transaction_status = 'PENDING';
+    //     }
+    //     else if ($status == 'deny') {
+    //         $transaction->transaction_status = 'FAILED';
+    //     }
+    //     else if ($status == 'expire') {
+    //         $transaction->transaction_status = 'EXPIRED';
+    //     }
+    //     else if ($status == 'cancel') {
+    //         $transaction->transaction_status = 'Failed';
+    //     }
+
+    //     //simpan transaction
+    //     $transaction->save();
+
+    //     //kirim email
+    //     if($transaction){
+    //         if ($status == 'capture' && $fraud == 'accept') {
+    //             Mail::to($transaction->user)->send(
+    //                 new TransactionSuccess($transaction)
+    //             );
+    //         }
+    //         else if ($status == 'settlement') {
+    //             Mail::to($transaction->user)->send(
+    //                 new TransactionSuccess($transaction)
+    //             );
+    //         }
+    //         else if ($status == 'success') {
+    //             Mail::to($transaction->user)->send(
+    //                 new TransactionSuccess($transaction)
+    //             );
+    //         }
+    //         else if ($status == 'capture' && $fraud == 'challenge') {
+    //             return response()->json([
+    //             'meta' => [
+    //                 'code' =>200,
+    //                 'message' => 'Midtrans Payment Challenge'
+    //                 ]
+    //             ]);
+    //         }
+    //         else{
+    //             return response()->json([
+    //                 'meta' => [
+    //                     'code' => 200,
+    //                     'message' => 'Midtrans Payment Not Settlement'
+    //                 ]
+    //             ]);
+    //         }
+    //     }
+
+    //     return response()->json([
+    //         'meta' => [
+    //             'code' => 200,
+    //             'message' => 'Midtrans Payment Success'
+    //         ]
+    //     ]);
+    // }
+
+    public function notificationHandler(Request $request)
+    {
         Config::$serverKey = config('midtrans.serverKey');
         Config::$isProduction = config('midtrans.isProduction');
         Config::$isSanitized = config('midtrans.isSanitized');
         Config::$is3ds = config('midtrans.is3ds');
 
-        //buat instance midtrans notification
         $notification = new Notification();
 
-        //pisah order id dari string
-        $order = explode('-', $notification->order_id);
+        return response()->json($notification);
 
-        //assign ke variable
-        $status = $notification->transaction_status;
-        $type = $notification->payment_type;
-        $fraud = $notification->fraud_status;
-        $order_id = $order[1];
-
-        //cari transaksi berdasarkan id
-        $transaction = Transaction::findOrFail($order_id);
-
-        //handle notification status midtrans
-        if($status == 'capture'){
-            if($type == 'credit_card'){
-                if($fraud == 'challenge'){
-                    $transaction->transaction_status = 'CHALLENGE';
-                } else{
-                    $transaction->transaction_status = 'SUCCESS';
-                }
-            }
-        }
-        else if ($status == 'settlement') {
-            $transaction->transaction_status = 'SUCCESS';
-        }
-        else if ($status == 'pending') {
-            $transaction->transaction_status = 'PENDING';
-        }
-        else if ($status == 'deny') {
-            $transaction->transaction_status = 'FAILED';
-        }
-        else if ($status == 'expire') {
-            $transaction->transaction_status = 'EXPIRED';
-        }
-        else if ($status == 'cancel') {
-            $transaction->transaction_status = 'Failed';
-        }
-
-        //simpan transaction
-        $transaction->save();
-
-        //kirim email
-        if($transaction){
-            if ($status == 'capture' && $fraud == 'accept') {
-                Mail::to($transaction->user)->send(
-                    new TransactionSuccess($transaction)
-                );
-            }
-            else if ($status == 'settlement') {
-                Mail::to($transaction->user)->send(
-                    new TransactionSuccess($transaction)
-                );
-            }
-            else if ($status == 'success') {
-                Mail::to($transaction->user)->send(
-                    new TransactionSuccess($transaction)
-                );
-            }
-            else if ($status == 'capture' && $fraud == 'challenge') {
-                return response()->json([
-                'meta' => [
-                    'code' =>200,
-                    'message' => 'Midtrans Payment Challenge'
-                    ]
-                ]);
-            }
-            else{
-                return response()->json([
-                    'meta' => [
-                        'code' => 200,
-                        'message' => 'Midtrans Payment Not Settlement'
-                    ]
-                ]);
-            }
-        }
-
-        return response()->json([
-            'meta' => [
-                'code' => 200,
-                'message' => 'Midtrans Payment Success'
-            ]
-        ]);
     }
 
     public function finishRedirect(Request $request)
