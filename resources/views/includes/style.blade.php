79b109d74b337d79eae74be132ffe59b4654746a
elanghairilanam <elang6414@gmail.com>
2023-01-02 16:37:49 +0700

    Add all my files

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,3 +1,2 @@
 <link rel="stylesheet" href="{{ url('frontend/libraries/bootstrap/css/bootstrap.css') }}">
 <link rel="stylesheet" href="{{ url('frontend/styles/main.css') }}">
-<script src="{{ url('frontend/libraries/jquery/jquery-3..4.1.min.js') }}"></script>
